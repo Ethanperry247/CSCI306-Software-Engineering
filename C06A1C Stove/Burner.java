317558f0bdd4c647e84f0f038e5cadc0b9c59466
srunningrabbit <srunningrabbit@mymail.mines.edu>
2020-01-26 22:37:43 -0700

    Added burner display method

--- C06A1C Stove/Burner.java
+++ C06A1C Stove/Burner.java
@@ -1,8 +1,8 @@
 
 public class Burner {
-	private enum Temperature { BLAZING, HOT, WARM, COLD };
+	public enum Temperature { BLAZING, HOT, WARM, COLD };
 	private Temperature myTemperature = Burner.Temperature.COLD;
-	private Setting mySetting = Setting.LOW;
+	private Setting mySetting = Setting.OFF;
 	private int timer = 0;
 	private static final int TIME_DURATION = 2;
 	
@@ -22,7 +22,9 @@ public class Burner {
 				mySetting = Setting.LOW;
 				break;
 		}
-		timer += TIME_DURATION;
+		if (timer == 0) {
+			timer += TIME_DURATION;
+		}
 	}
 	
 	public void minusButton() {
@@ -37,31 +39,47 @@ public class Burner {
 				mySetting = Setting.OFF;
 				break;
 		}
-		timer += TIME_DURATION;
+		if (timer == 0) {
+			timer += TIME_DURATION;
+		}
 	}
 	
 	public void updateTemperature() {
 		if (timer == 0) {
 			switch (mySetting) {
-			case HIGH:
-				myTemperature = Burner.Temperature.BLAZING;
-				break;
-			case MEDIUM:
-				myTemperature = Burner.Temperature.HOT;
-				break;
-			case LOW:
-				myTemperature = Burner.Temperature.WARM;
-				break;
-			case OFF:
-				myTemperature = Burner.Temperature.COLD;
-				break;
-		}
+				case HIGH:
+					myTemperature = Burner.Temperature.BLAZING;
+					break;
+				case MEDIUM:
+					myTemperature = Burner.Temperature.HOT;
+					break;
+				case LOW:
+					myTemperature = Burner.Temperature.WARM;
+					break;
+				case OFF:
+					myTemperature = Burner.Temperature.COLD;
+					break;
+			}
 		} else {
 			timer--;
 		}
 	}
 
 	public void display() {
-		System.out.println(mySetting.toString());
+		System.out.print(mySetting.toString());
+		switch (myTemperature) {
+			case BLAZING:
+				System.out.println("VERY HOT! DON'T TOUCH");
+				break;
+			case HOT:
+				System.out.println("CAREFUL");
+				break;
+			case WARM:
+				System.out.println("warm");
+				break;
+			case COLD:
+				System.out.println("cooool");
+				break;
+		}
 	}
 }
